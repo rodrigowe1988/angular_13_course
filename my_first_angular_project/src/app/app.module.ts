16b62f7abd1c7f70deecad16885b2e7f472f31b7
Rodrigo Weber <rodrigoweber@pop-os.localdomain>
2022-08-11 21:25:22 -0300

    sharing data @Input

--- my_first_angular_project/src/app/app.module.ts
+++ my_first_angular_project/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TesteComponent } from './components/teste/teste.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TesteComponent,
+    ParentDataComponent,
   ],
   imports: [
     BrowserModule
