49e5bc26852f2489b2884371411526b4e5cb37dc
Rodrigo Weber <rodrigoweber@pop-os.localdomain>
2022-08-15 07:09:42 -0300

    working with interfaces

--- my_first_angular_project/src/app/components/list-render/list-render.component.ts
+++ my_first_angular_project/src/app/components/list-render/list-render.component.ts
@@ -1,19 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Animal } from 'src/app/Animal';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.css'],
 })
 export class ListRenderComponent implements OnInit {
-  animals = [
-    { name: 'Jerry', type: 'Mouse' },
-    { name: 'Tom', type: 'Cat' },
-    { name: 'Spyke', type: 'Dog' },
-    { name: 'Piu-Piu', type: 'Bird' },
+  animals: Animal[] = [
+    { name: 'Jerry', type: 'Mouse', age: 1 },
+    { name: 'Tom', type: 'Cat', age: 2 },
+    { name: 'Spyke', type: 'Dog', age: 4 },
+    { name: 'Piu-Piu', type: 'Bird', age: 1 },
   ];
+
+  animalDetails = '';
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  showAge(animal: Animal): void {
+    this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos.`;
+  }
 }
--- my_first_angular_project/src/app/components/table-list-render/table-list-render.component.ts
+++ my_first_angular_project/src/app/components/table-list-render/table-list-render.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Animal } from 'src/app/Animal';
+
 @Component({
   selector: 'app-table-list-render',
   templateUrl: './table-list-render.component.html',
   styleUrls: ['./table-list-render.component.css'],
 })
 export class TableListRenderComponent implements OnInit {
-  animals = [
-    { name: 'Jerry', type: 'Mouse' },
-    { name: 'Tom', type: 'Cat' },
-    { name: 'Spyke', type: 'Dog' },
-    { name: 'Piu-Piu', type: 'Bird' },
+  animals: Animal[] = [
+    { name: 'Jerry', type: 'Mouse' , age: 1},
+    { name: 'Tom', type: 'Cat' , age: 2},
+    { name: 'Spyke', type: 'Dog', age: 4},
+    { name: 'Piu-Piu', type: 'Bird', age: 1},
   ];
 
   constructor() {}
