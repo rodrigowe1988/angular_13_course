a95bb4335c74acdc8665c62726ccbe3049b9267e
Rodrigo Weber <rodrigoweber@MacBook-Pro-de-Rodrigo.local>
2023-01-14 14:47:31 -0300

    trabalhando com FORMS

--- my_first_angular_project/src/app/components/forms/forms.component.ts
+++ my_first_angular_project/src/app/components/forms/forms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormsComponent implements OnInit {
 
+public comidas : Array<{comida: string, preco: string}> = [
+  {comida: "pizza", preco: 'R$15,00'},
+  {comida: 'lasanha', preco: 'R$17,00'},
+  {comida: 'saladas', preco: 'R$10,00'},
+  {comida: 'frutas', preco: 'R$8,00'}
+]
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public submitForm(form: NgForm) {
+    console.log(form.value);
   }
 
 }
